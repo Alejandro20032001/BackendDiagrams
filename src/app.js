4afb46473f84c457053d2388144ce7e0f45e14b4
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-14 20:11:21 -0400

    configuracion para que acepte json

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from 'express'
 import morgan from 'morgan'
 import userRoutes from './routes/user.routes'
 const app = express()
+
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json('welcome')
