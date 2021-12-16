3ea57eddd21901c3775bb71aeaf2b2411a5fb7df
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-16 19:49:05 -0400

    metodos de encriptacion y validacion

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,13 +1,38 @@
+import bcrypt from "bcryptjs";
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
   {
-    completeName: String,
-    username: String,
-    password: String,
+    completeName: {
+      type: String,
+      unique: false,
+    },
+    username: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      //select: false,
+    },
+    roles:[{
+      ref: 'Role',
+      type: Schema.Types.ObjectId
+    }]
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.statics.encryptPass = async (password) => {
+  const salt = await bcrypt.genSalt(10)
+  return await bcrypt.hash(password, salt)
+};
+
+userSchema.statics.validatePass = async (password, receivedPass) => {
+  return await bcrypt.compare(password, receivedPass)
+};
+
 export default model("User", userSchema);
