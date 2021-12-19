0a9145d39c87b00bb41b9be33bee4fb3aaa97e3d
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-19 16:03:50 -0400

    rutas de las clases

---
+++ src/routes/clase.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as classController from "../controllers/clase.controller";
+const router = Router();
+
+router.post("/", classController.createClass);
+router.get("/", classController.getClasses);
+router.get("/classId", classController.getClassById);
+
+export default router;
