6d7dff3caa5b799cb4695159cfae9ac798b1fc64
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-19 16:03:31 -0400

    ruta de los diagrams

--- src/routes/diagram.routes.js
+++ src/routes/diagram.routes.js
@@ -1,5 +1,13 @@
-import {Router} from 'express'
+import { Router } from "express";
+import * as diagramController from "../controllers/diagram.controller"
 
-const router = Router()
+const router = Router();
 
+router.post('/', diagramController.createDiagram);
+
+router.put('/:diagramId', diagramController.updateDiagram)
+
+router.get('/:diagramId', diagramController.getDiagramById);
+
+router.get('/', diagramController.getAllDiagrams);
 export default router;
