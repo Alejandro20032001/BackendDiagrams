9ad60a63e292178e754920625f16a8c1a0fd9866
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-19 16:04:20 -0400

    se añadio acceso de diagramas

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -16,10 +16,18 @@ const userSchema = new Schema(
       required: true,
       //select: false,
     },
-    roles:[{
-      ref: 'Role',
-      type: Schema.Types.ObjectId
-    }]
+    roles: [
+      {
+        ref: "Role",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    diagramsDone:[
+      {
+        ref: "Diagrama",
+        type: Schema.Types.ObjectId
+      }
+    ]
   },
   {
     timestamps: true,
@@ -27,12 +35,12 @@ const userSchema = new Schema(
 );
 
 userSchema.statics.encryptPass = async (password) => {
-  const salt = await bcrypt.genSalt(10)
-  return await bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
 };
 
 userSchema.statics.validatePass = async (password, receivedPass) => {
-  return await bcrypt.compare(password, receivedPass)
+  return await bcrypt.compare(password, receivedPass);
 };
 
 export default model("User", userSchema);
