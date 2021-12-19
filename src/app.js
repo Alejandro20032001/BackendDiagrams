d3c0e2fe457b2aff3951e6552e0d49c6faf5ddf9
Alejandro20032001 <dilanantezama@gmail.com>
2021-12-19 16:06:53 -0400

    uses de las rutas

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
-import express from 'express'
-import morgan from 'morgan'
-import userRoutes from './routes/user.routes'
-import authRoutes from './routes/auth.routes'
-import classRoutes from './routes/clase.routes'
-import { createRoles } from './libs/initSetUp'
-const app = express()
-createRoles()
+import express from "express";
+import morgan from "morgan";
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
+import classRoutes from "./routes/clase.routes";
+import groupRoutes from "./routes/group.routes";
+import unionRoutes from "./routes/union.routes";
+import diagramRoutes from "./routes/diagram.routes";
+import { createRoles } from "./libs/initSetUp";
+const app = express();
+createRoles();
 
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json('welcome')
-})
+app.get("/", (req, res) => {
+  res.json("welcome");
+});
 
-app.use('/user',userRoutes)
-app.use('/auth', authRoutes)
-app.use('/class', classRoutes)
+app.use("/user", userRoutes);
+app.use("/auth", authRoutes);
+app.use("/class", classRoutes);
+app.use("/group", groupRoutes);
+app.use("/union", unionRoutes);
+app.use("/diagram", diagramRoutes);
 export default app;
